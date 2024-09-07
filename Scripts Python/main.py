840b4adec834955643d90484a435dac62eebc5d9
Ayrton Casa <ayrton.casa@sptech.school>
2024-09-07 18:45:25 -0300

    Sprint1.py pronto | Continuação de atualização futura

--- Scripts Python/main.py
+++ Scripts Python/main.py
@@ -2,11 +2,13 @@ from mysql.connector import connect, Error
 import psutil as p 
 import time
 import platform 
-from dotenv import load_dotenv
+from dotenv import load_dotenv as os
 
-load_dotenv()
+os.load_dotenv() 
 
 SO = platform.system()
+ipv4 = p.net_connections(kind='inet4')
+ipv6 = p.net_connections(kind='inet6')
 
 config = {
   "user": os.getenv("USER_LOGIN"),
@@ -15,6 +17,28 @@ config = {
   "database": os.getenv("DATABASE")
 }
 
+#quero pegar o ipv4, o ipv6 e o disco automaticamente, assim abstraindo pro usuário e colocar na tabela servidor
+try:
+            mydb = connect(**config)
+            if mydb.is_connected():
+
+                mycursor = mydb.cursor()
+
+                sql_query = "INSERT INTO Servidor() VALUES (%s, %s, %s)"
+                val = []
+                mycursor.execute(sql_query, val)
+                mydb.commit()
+                print(mycursor.rowcount, "registro inserido")
+
+except Error as e:
+        print("Erro ao conectar com o MySQL", e)
+        
+finally:
+        if(mydb.is_connected()):
+            mycursor.close()
+            mydb.close()
+
+#aqui começa a captura de dados
 i = 0
 while i < 10:
     percent_cpu = p.cpu_percent()
@@ -46,24 +70,26 @@ try:
                     mydb.commit()
 
                     #select pra pegar idDado
-
-
+                    result = mycursor.execute("SELECT idDado FROM Dado ORDER BY idDado DESC LIMIT = 1")
+                    idDado = mycursor.fetchall()
+                    print(idDado) 
+                    
 
                     if(percent_cpu > 70.0):
-                        sql_query = "INSERT INTO Alerta VALUES (1, %s)"
-                        val = [percent_cpu]
+                        sql_query = "INSERT INTO Alerta VALUES (%s, 'Porcentage de cpu em risco: %s%')"
+                        val = [idDado, percent_cpu]
                         mycursor.execute(sql_query, val)
                         mydb.commit()
 
                     if(percent_memory > 70.0):
-                        sql_query = "INSERT INTO Alerta VALUES (%s)"
-                        val = [percent_memory]
+                        sql_query = "INSERT INTO Alerta VALUES (%s, 'Porcentage de memória em risco: %s%')"
+                        val = [idDado, percent_memory]
                         mycursor.execute(sql_query, val)
                         mydb.commit()
 
                     if(percent_disk.percent > 75.0):
-                        sql_query = "INSERT INTO Alerta VALUES (%s)"
-                        val = [percent_disk.percent]
+                        sql_query = "INSERT INTO Alerta VALUES (%s, 'Porcentage de disco em risco: %s%')"
+                        val = [idDado, percent_disk.percent]
                         mycursor.execute(sql_query, val)
                         mydb.commit()
 
