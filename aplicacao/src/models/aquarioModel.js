c14153cf9f18fbf686cf38b59fb3046321bc722f
W-Faria-M <gabriel.barranco@sptech.school>
2024-10-03 18:24:26 -0300

    Configurações na listagem de servidores

--- aplicacao/src/models/aquarioModel.js
+++ aplicacao/src/models/aquarioModel.js
@@ -18,19 +18,7 @@ function listarServidores(idEmpresa) {
   console.log(
     "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
   );
-  var instrucaoSql = `
-        select 
-          e.razaoSocial as empresa,
-          s.id as id,
-          sv.nome as servicoMonitorado,
-          r.valor as valorRegistro
-            from empresa e
-            join servidor s on e.id = s.fkEmpresa
-            join servicoMonitorado sm on s.id = sm.fkServidor
-            join servico sv on sm.fkServico = sv.id
-            join registro r on sm.id = r.fkServicoMonitorado
-              where e.id = ${idEmpresa};
-    `;
+  var instrucaoSql = `select * from vw_servidor_card where id_empresa = ${idEmpresa};`;
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
