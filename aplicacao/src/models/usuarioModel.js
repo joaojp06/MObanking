ae52997ef4b1075499b508cfd6b46d1d8f0e0bc3
gvjuliamaria <julia.almeida@sptech.school>
2024-10-17 00:37:03 -0300

    UP: Correção da senha undefined ao cadastrar novo usuário.

--- aplicacao/src/models/usuarioModel.js
+++ aplicacao/src/models/usuarioModel.js
@@ -10,15 +10,17 @@ function autenticar(email, senha) {
 }
 
 
-function cadastrar(nome, email, cpf,nivel, fkEmpresa, nivel, senha) {
+function cadastrar(nome, email, cpf,senha, fkEmpresa, nivel) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente.");
+    console.log("Parâmetros recebidos:", { nome, email, cpf, senha, fkEmpresa, nivel });
 
     var instrucaoSql = `
         insert into usuario (fkEmpresa, fkTipoUsuario, nome, cpf, email, senha, status)
-        VALUES ('${fkEmpresa}','${nivel}','${nome}', '${cpf}', '${email}', '${senha}', 'ativo');
+        VALUES ('${fkEmpresa}','${nivel}','${nome}', '${cpf}', '${email}', '${senha}', 'Ativo');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
+    
 }
 
 function listarFuncionarios(idEmpresa, idUsuarioListFun) {
