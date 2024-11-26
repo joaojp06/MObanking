407bb42b66424c466298179d59fc0567a13f8600
joaojp06 <joao.antonio@sptech.school>
2024-11-26 14:29:43 -0300

    Individual - ADD: KPI funcionando. UP: Alteração da disposição dos elementos dos cards

--- aplicacao/src/models/joaoModel.js
+++ aplicacao/src/models/joaoModel.js
@@ -1,6 +1,144 @@
 var database = require("../database/config");
 
+function listarPrevisoes(idEmpresa, status) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  );
+  var instrucaoSql = `SELECT 
+  s.apelido,
+  s.funcao,
+  r.valor,
+  r.fkAlertaPrevisao
+FROM 
+  servidor s
+JOIN 
+  registro r ON r.fkServidor = s.id
+WHERE 
+  r.fkServico = 5 
+  AND s.fkEmpresa = ${idEmpresa}
+  AND r.data = (
+      SELECT MAX(r2.data)
+      FROM registro r2
+      WHERE r2.fkServidor = s.id
+        AND r2.fkServico = 5
+  )
+  ORDER BY 
+  r.valor DESC;`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function listarVerde(idEmpresa, status) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  );
+  var instrucaoSql = `SELECT 
+  COUNT(*) AS quantidade
+FROM 
+  (
+      SELECT 
+          s.apelido, 
+          s.funcao,
+          r.valor AS ultimo_registro,
+          r.fkAlertaPrevisao
+      FROM 
+          servidor s
+      JOIN 
+          empresa e ON s.fkEmpresa = e.id
+      LEFT JOIN 
+          registro r ON s.id = r.fkServidor 
+          AND r.fkServico = 5
+          AND r.data = (
+              SELECT MAX(data) 
+              FROM registro 
+              WHERE fkServidor = s.id 
+              AND fkServico = 5
+          )
+      WHERE 
+          e.id = ${idEmpresa}
+  ) AS subquery
+WHERE 
+  fkAlertaPrevisao = 1;
+`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function listarAmarelo(idEmpresa, status) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  );
+  var instrucaoSql = `SELECT 
+  COUNT(*) AS quantidade
+FROM 
+  (
+      SELECT 
+          s.apelido, 
+          s.funcao,
+          r.valor AS ultimo_registro,
+          r.fkAlertaPrevisao
+      FROM 
+          servidor s
+      JOIN 
+          empresa e ON s.fkEmpresa = e.id
+      LEFT JOIN 
+          registro r ON s.id = r.fkServidor 
+          AND r.fkServico = 5
+          AND r.data = (
+              SELECT MAX(data) 
+              FROM registro 
+              WHERE fkServidor = s.id 
+              AND fkServico = 5
+          )
+      WHERE 
+          e.id = ${idEmpresa}
+  ) AS subquery
+WHERE 
+  fkAlertaPrevisao = 2;
+`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function listarVermelho(idEmpresa, status) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  );
+  var instrucaoSql = `SELECT 
+  COUNT(*) AS quantidade
+FROM 
+  (
+      SELECT 
+          s.apelido, 
+          s.funcao,
+          r.valor AS ultimo_registro,
+          r.fkAlertaPrevisao
+      FROM 
+          servidor s
+      JOIN 
+          empresa e ON s.fkEmpresa = e.id
+      LEFT JOIN 
+          registro r ON s.id = r.fkServidor 
+          AND r.fkServico = 5
+          AND r.data = (
+              SELECT MAX(data) 
+              FROM registro 
+              WHERE fkServidor = s.id 
+              AND fkServico = 5
+          )
+      WHERE 
+          e.id = ${idEmpresa}
+  ) AS subquery
+WHERE 
+  fkAlertaPrevisao = 3;
+`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
 
 module.exports = {
-
-  };
+  listarPrevisoes,
+  listarVerde,
+  listarAmarelo,
+  listarVermelho
+};
--- aplicacao/src/routes/joao.js
+++ aplicacao/src/routes/joao.js
@@ -3,6 +3,21 @@ var router = express.Router();
 
 var joaoController = require("../controllers/joaoController");
 
+router.get("/listarPrevisoes/:idEmpresa/:status", function (req, res) {
+    joaoController.listarPrevisoes(req, res);
+})
 
+router.get("/listarVerde/:idEmpresa/:status", function (req, res) {
+    joaoController.listarVerde(req, res);
+})
+
+router.get("/listarAmarelo/:idEmpresa/:status", function (req, res) {
+    joaoController.listarAmarelo(req, res);
+})
+
+
+router.get("/listarVermelho/:idEmpresa/:status", function (req, res) {
+    joaoController.listarVermelho(req, res);
+})
 
 module.exports = router;
