086f78b32f4fc3302aab18e27623715eec61127c
Kaua-Navarro <kaua.navarro@sptech.school>
2024-11-28 13:51:26 -0300

    Dashboard Individual - Quantidade de Alertas por Servidor/Componente - Progresso - 50%

--- aplicacao/src/controllers/kauaController.js
+++ aplicacao/src/controllers/kauaController.js
@@ -1,6 +1,22 @@
 var kauaModel = require("../models/kauaModel");
 
+function listarServidores(req, res) {
+  var idEmpresa = req.params.idEmpresa;
+  var mes = req.params.mes;
+
+  kauaModel.listarServidores(idEmpresa, mes).then(function (resultado) {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(204).send("Nenhum resultado encontrado!")
+    }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao buscar os servidores avisos: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
 
 module.exports = {
-
+    listarServidores
   }
