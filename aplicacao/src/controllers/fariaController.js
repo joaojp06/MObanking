8689c9054d03ff5c80fe6983604b9f68dc5d5030
W-Faria-M <gabriel.barranco@sptech.school>
2024-12-03 08:09:52 -0300

    Gabriel de Faria - INDIVIDUAL - UP: Implementação dos dois fetchs para buscar dados no banco de dados realizada com sucesso.

--- aplicacao/src/controllers/fariaController.js
+++ aplicacao/src/controllers/fariaController.js
@@ -1,6 +1,34 @@
 var fariaModel = require("../models/fariaModel");
 
+function dadosGrafico(req, res) {
+  fariaModel.dadosGrafico().then(function (resultado) {
+      if (resultado.length > 0) {
+          res.status(200).json(resultado);
+      } else {
+          res.status(204).send("Nenhum resultado encontrado!")
+      }
+  }).catch(function (erro) {
+      console.log(erro);
+      console.log("Houve um erro ao buscar os dados do gráfico: ", erro.sqlMessage);
+      res.status(500).json(erro.sqlMessage);
+  });
+}
+
+function obterIndicadores(req, res) {
+  fariaModel.obterIndicadores().then(function (resultado) {
+      if (resultado.length > 0) {
+          res.status(200).json(resultado);
+      } else {
+          res.status(204).send("Nenhum resultado encontrado!")
+      }
+  }).catch(function (erro) {
+      console.log(erro);
+      console.log("Houve um erro ao buscar os dados do gráfico: ", erro.sqlMessage);
+      res.status(500).json(erro.sqlMessage);
+  });
+}
 
 module.exports = {
-
-  }
+  dadosGrafico,
+  obterIndicadores
+};
--- aplicacao/src/routes/faria.js
+++ aplicacao/src/routes/faria.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 var fariaController = require("../controllers/fariaController");
 
+router.get("/dadosGrafico", function (req, res) {
+    fariaController.dadosGrafico(req, res);
+});
 
+router.get("/obterIndicadores", function (req, res) {
+    fariaController.obterIndicadores(req, res);
+});
 
 module.exports = router;
