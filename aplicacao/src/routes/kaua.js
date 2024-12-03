356481f5d9b0d3285b34697db79b95079042fd82
Kaua-Navarro <kaua.navarro@sptech.school>
2024-12-03 19:20:44 -0300

    Projeto Individual PRONTO

--- aplicacao/src/routes/kaua.js
+++ aplicacao/src/routes/kaua.js
@@ -7,5 +7,21 @@ router.get("/listarServidores/:idEmpresa/:mes", function (req, res) {
     kauaController.listarServidores(req, res);
 })
 
+router.get("/obterQtdAlertasCPU/:idServidorPar/:mes/:idEmpresa", function (req, res) {
+    kauaController.obterQtdAlertasCPU(req, res);
+})
+
+router.get("/obterQtdAlertasRAM/:idServidorPar/:mes/:idEmpresa", function (req, res) {
+    kauaController.obterQtdAlertasRAM(req, res);
+})
+
+router.get("/obterQtdAlertasDISCO/:idServidorPar/:mes/:idEmpresa", function (req, res) {
+    kauaController.obterQtdAlertasDISCO(req, res);
+})
+
+router.get("/obterQtdAlertasREDE/:idServidorPar/:mes/:idEmpresa", function (req, res) {
+    kauaController.obterQtdAlertasREDE(req, res);
+})
+
 
 module.exports = router;
